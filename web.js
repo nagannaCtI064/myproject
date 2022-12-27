6e7119be267f311a68e39f6010bc94e4bcbb3b7d
nagannaCtI064 <119935869+nagannaCtI064@users.noreply.github.com>
2022-12-27 10:18:49 +0530

    i uploaded my website

---
+++ web.js
@@ -0,0 +1,7 @@
+var card=document.getElementById("card");
+function openRegister(){
+    card.style.transform="rotateY(-180deg)";
+}
+function openlogin(){
+    card.style.transform="rotateY(0deg)";
+}
